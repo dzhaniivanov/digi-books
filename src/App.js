19a7b82bd0e6277971251b75608d1210dff2bfe5
dzhani ivanov <djani.ivanov94@gmail.com>
2021-11-21 11:05:28 +0200

    try to add persisting state with redux persist

--- src/App.js
+++ src/App.js
@@ -8,19 +8,20 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import SingleBookPage from "./components/SingleBookPage";
 import Settings from "./components/Settings";
-import { useSelector } from "react-redux";
+import PrivateRoute from "./components/PrivateRoute";
 
 
 
 function App() {
-  //const user=useSelector(state=>state.user.currentUser);
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" exact element={<PrivateRoute />} >
+          <Route exact path="/" element={<Home />} />
+        </Route>
         <Route path="/login" exact element={<Login />} />
         <Route path="/register" exact element={<Register />} />
-        <Route path="/book/:id" exact element={<SingleBookPage />}  />
+        <Route path="/book/:id" exact element={<SingleBookPage />} />
         <Route path="/settings" exact element={<Settings />} />
       </Routes>
     </Router>
---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+
+const PrivateRoute = () => {
+    const user = useSelector(state => state.user.currentUser);
+    return user ? <Outlet /> : <Navigate to="/login" />;
+}
+
+
+export default PrivateRoute;
